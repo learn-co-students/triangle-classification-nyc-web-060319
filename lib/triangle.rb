bdc5e3bbddebe54cc43096a4752bb744a0c12fe0
CindyChenx <chen_cx0128@hotmail.com>
2019-06-12 16:27:09 -0400

    half done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,26 @@
 class Triangle
   # write code here
   attr_accessor :equilateral, :isosceles, :scalene
+  attr_reader :a, :b, :c 
 
-  def initialize()
-  
+  def initialize(a,b,c)
+    @a = a 
+    @b = b 
+    @c = c
   end
 
   def kind 
+    if a == b && b == c  
+      @equilateral 
+    elsif a == b || a == c || b == c  
+      :isosceles 
+    else 
+      :scalene 
+    end
+  end
 
+  def validated? 
+    
   end
 
   def TriangleError < StandardError
